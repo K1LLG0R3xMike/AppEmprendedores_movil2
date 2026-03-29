dc53090e0cb030d2e158de8a166e6fa4ac82219e
Miguel Angel Rodriguez C. <118031789+K1LLG0R3xMike@users.noreply.github.com>
2026-03-29 00:20:36 -0500

    variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   firebaseApiKey: 'AIzaSyA1yWyrDp_wlwZflfgKmw4SSnP7cTbU1Fs', // Reemplaza con tu API key real
-  apiBaseUrl: 'https://your-production-api.com/api', // Cambia por tu URL de producción
-  aiServiceBaseUrl: 'https://your-ai-service-domain.com'
+  apiBaseUrl: 'https://back-appemprendedores.onrender.com/api',
+  aiServiceBaseUrl: 'https://ia-service-mk5h.onrender.com/'
 };
